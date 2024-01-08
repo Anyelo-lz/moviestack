bde2065939e1d2d7c7e1c01a846b6b020e2f6e44
angel <angelwilliamleonel.lz@gmail.com>
2024-01-08 00:08:18 -0300

    mejoras de la pagina en peliculas con su filtros de Genero y Busquedas con un pequeño cambio de estilo

--- main.js
+++ main.js
@@ -1,29 +1,58 @@
 console.log(moviestack)
 const traerMovie = document.getElementById("movieTwo")
-console.log(traerMovie)
-
-const elementosCard = (title, image, homepage, overview, release_date, runtime, vote_average, vote_count) => {
+const elementosCard = (title, image, homepage, overview) => {
     return `
-    <div class="w-2/4 ">
-     <section class="p-10">
-    <h3 class="text-white  m-5 text-2xl text-center">${title}</h3>
-    <a href="${homepage}"><img class=" mx-5 block w-auto" src="${image}" alt=""></a>
-    <p class="text-white  text-xs my-5 ml-10">${overview}</p>
-    <p class="text-white mx-2 ml-5 ">Vote average: ${vote_average}</p>
-    <p class="text-white mx-2 ml-5">Vote count: ${vote_count}</p>
-    <p class="text-white mx-2 ml-5">Release date: ${release_date}</p>
-    <p class="text-white mx-2 ml-5 ">Runtime: ${runtime} min.</p>
+    <div class="w-1/4 border rounded-3xl border-dashed border-4 border-violet-600">
+     <section class="p-5">
+    <h3 class="text-white  m-5 text-lg text-center">${title}</h3>
+    <a href="${homepage}"><img class="pr-10 m-5 " src="${image}" alt=""></a>
+    <p class="text-white text-justify text-xs ">${overview}</p>
     </section>
-    </div>
-    `
+    </div>`
 }
 const recorriendotwocard = (elementos) => {
-    let $contadortwo = ""
+    let $contadortwo = " "
     for (const recorridotwo of elementos) {
-        $contadortwo += elementosCard(recorridotwo.title, recorridotwo.image, recorridotwo.homepage,recorridotwo.overview, 
-        recorridotwo.release_date, recorridotwo.runtime, recorridotwo.vote_average, recorridotwo.vote_count)
+        $contadortwo += elementosCard(recorridotwo.title, recorridotwo.image, recorridotwo.homepage,recorridotwo.overview
+        ) 
     }
-    return $contadortwo
+    return $contadortwo 
 }
-traerMovie.innerHTML = (recorriendotwocard(moviestack))
+traerMovie.innerHTML=recorriendotwocard(moviestack)
+//----------------------------------------------------------------
 
+// evento de genero de la lista desplegable
+/*GeneroMovie.addEventListener("change",(e) =>{
+    console.log(e.target.value)
+    
+})*/
+//evento de input por teclado
+const $busquedaContenido=document.getElementById("busqueda")
+$busquedaContenido.addEventListener("input",(text)=>{
+    console.log($busquedaContenido.value)
+    const filtromoviefilm =filtarPelisPorNombres(moviestack, $busquedaContenido.value)
+    traerMovie.innerHTML=recorriendotwocard(filtromoviefilm)//fltracion con el evento del input
+})
+
+
+//----------------------filtro de las peliculas------------------
+function filtarPelisPorNombres(listmoviestack,titleMovie){
+    return listmoviestack.filter(moviest=>moviest.title.toLowerCase().includes(titleMovie.toLowerCase()))
+    
+}
+
+//--------------evento de Genero-------------------------
+const $GeneroMovie = document.getElementById("Genero")
+$GeneroMovie.addEventListener("change", (e) => {
+    const generoSeleccionado = e.target.value
+    const peliculasFiltradas = seleccionGenero(moviestack, generoSeleccionado)
+    traerMovie.innerHTML = recorriendotwocard(peliculasFiltradas)
+})
+
+//---------------filtro por genero-------------------------------------
+function seleccionGenero(listmovi, genero) {
+    return listmovi.filter(movie => movie.genres.includes(genero))
+}
+
+
+ 
